2fa1606b39cbb735ace138a7d3edb781bbde35d7
Lyndon Tremblay <quakerquickoats@gmail.com>
2017-02-14 23:45:44 -0500

    add sdl2/opengl test

---
+++ wanderman.el
@@ -0,0 +1,9 @@
+;; -*- lexical-binding: t; -*-
+
+(require 'cl)
+
+;; (cl-defstruct wm:actor
+;;   level status inventory equipment shkel)
+;; (cl-defstruct wm:state
+;;   wanderer actors map story)
+
