a0a522c97af886f6242e061c3d3ead346d4a088a
Lyndon Tremblay <quakerquickoats@gmail.com>
2018-08-06 19:50:16 -0400

    ok

---
+++ esprit.el
@@ -0,0 +1,14 @@
+;;; -*- lexical-binding: t -*-
+;;; emascs sprite editor
+;;;
+;;;
+;;; 
+;;; artist-mode
+;;;
+;;; 
+;;;
+;;; picture-mode
+;;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Picture-Mode.html#index-pictures-4431
+
+
+;;; tetris-mode
