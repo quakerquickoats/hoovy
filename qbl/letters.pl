3a1df3a0985f0ce9531aa3f1554e9f4d20d63c5d
Lyndon Tremblay <quakerquickoats@gmail.com>
2018-10-08 12:03:54 -0400

    sorting out..

--- qbl/letters.pl
+++ qbl/letters.pl
@@ -1,50 +1,82 @@
-%% Gamelike
+%% Qabala
 %%
 %% (c) 2018 Lyndon Tremblay
 %%
 
-:- module(qabala,[]).
+:- encoding(utf8).
+:- module(qabala,[letters/1,
+                  values/1,
+                  letterValue/2]).
 
 letters(L) :-
-	L = [aleph,gimel,beth].
+	L = [alef,beth,gimel,dalet,he,waw,zayn,chet,tet,
+         yod,kaf,lamed,mem,nun,samek,ayin,peh,cadi,
+         qof,resh,shin,tau,'Kaf','Mem','Nun','Peh','Cadi'].
 
-letter(a,1).
-letter(b,2).
-letter(g,3).
-letter(d,4).
-letter(e,5).
-letter(w,6).
-letter(z,7).
-letter(h,8).
-letter(t,9).
+blah :- [a,b,c,d].
 
-letter(i,10).
-letter(k,20).
-letter(l,30).
-letter(m,40).
-letter(n,50).
-letter(o,60).
-letter(y,70).
-letter(p,80).
-letter(c,90).
+values(V) :-
+	V = [1,2,3,4,5,6,7,8,9,
+	     10,20,30,40,50,60,70,80,90,
+	     100,200,300,400,500,600,700,800,900].
 
-letter(q,100).
-letter(r,200).
-letter(s,300).
-letter(x,400).
-letter('K',500).
-letter('M',600).
-letter('N',700).
-letter('P',800).
-letter('C',900).
+letterValue(L,V) :-
+    letters(Ls),
+    values(Vs),
+    nth0(N,Ls,L).
+%    nth0(N,Vs,V),
+%    V.
 
-letterValues :-
-	numlist(1,9,Ten),
-	[1,2,3,4,5,6,7,8,9,
-	 10,20,30,40,50,60,70,80,90,
-	 100,200,300,400,500,600,700,800,900].
+letter(alef,C) :- C = (1,(yod,waw,yod)).
 
-letter('a') :- 
+alef(1,'a').
+beth(2,'b').
+gimel(3,'g').
+dalet().
+
+alef([yod,waw,yod]).
+beth([resh,waw]).
+
+letters(1) :- alef.
+letters(2) :- alef, alef.
+letters(2) :- beth.
+letters(3) :- alef, alef, alef.
+letters(3) :- alef, beth.
+letters(3) :- gimel.
+
+letters(26) :- yod, he, waw, he.
+
+%%%%%%%%%%%%%%%%%%%%
+
+letter(alef,1).
+letter(beth,2).
+letter(gimel,3).
+letter(dalet,4).
+letter(he,5).
+letter(waw,6).
+letter(zayn,7).
+letter(chet,8).
+letter(tet,9).
+
+letter(yod,10).
+letter(kaf,20).
+letter(lamed,30).
+letter(mem,40).
+letter(nun,50).
+letter(samek,60).
+letter(ayin,70).
+letter(peh,80).
+letter(cadi,90).
+
+letter(qof,100).
+letter(resh,200).
+letter(shin,300).
+letter(tau,400).
+letter('Kaf',500).
+letter('Mem',600).
+letter('Nun',700).
+letter('Peh',800).
+letter('Cadi',900).
 
 letterValues(String,Values):-
 	maplist(letter,string_codes(String),Values).
