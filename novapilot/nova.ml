02f00b3dfbbe8de60b226721aed4729d75bdb48a
Lyndon Tremblay <quakerquickoats@gmail.com>
2019-05-11 18:17:21 -0400

    cairo text sorta

--- novapilot/nova.ml
+++ novapilot/nova.ml
@@ -4,9 +4,16 @@
  *)
 
 module Make(C: JsOfOCairo.S) = struct
-  let draw ctx =
-    C.save ctx;
-    C.arc ctx 50. 50. ~r:40. ~a1:0. ~a2:5.;
-    C.stroke ctx;
-    C.restore ctx
+  let draw c =
+    C.save c;
+    C.arc c 50. 50. ~r:40. ~a1:0. ~a2:5.;
+    C.stroke c;
+
+    C.set_source_rgb c 1. 1. 1.;
+    C.select_font_face c "monospace";
+    C.set_font_size c 20.;
+    C.move_to c 22. 22.;
+    C.show_text c "אלף-בית";
+
+    C.restore c
 end
--- novapilot/novapilot.ml
+++ novapilot/novapilot.ml
@@ -3,48 +3,43 @@
   (c) 2019 Lyndon Tremblay
  *)
 
-open Nova
 open System_glfw
+(* open Tgl3 *)
 
-open Foreign
-open Ctypes
-
-module Nanovg = struct
-  let createGL3 = foreign "nvgCreateGL3" (int @-> returning (ptr int))
-end
+module Nova = Nova.Make(Cairo)
 
 let () =
   let w, h = 320, 240 in
   let s = System.init w h "Novapilot" in
 
-  (*
-  let i = Cairo.Image.create Cairo.Image.ARGB32 ~w ~h in
+  let i = Cairo.Image.(create ARGB32 ~w ~h) in
   let c = Cairo.create i in
-  Cairo.scale c 1. 1.; *)
-  
+  Cairo.scale c 1. 1.;
+
+  Nova.draw c;
   (*  let fbo = Bigarray.(Array1.create Int32 c_layout 1) in
   Gl.gen_framebuffers 1 fbo;*)
   (* let tex = Nova.createTexture w h i in
   let (gID,_) = Nova.createModel () in *)
   (* let fb = Nova.createFramebuffer w h in *)
 
-  let vg = Nanovg.createGL3 0 in
-  print_int (!@ vg);
-  print_newline ();
-
   while System.update s do
-    Nova.prepareScene w h;
-    (* Nova.bindTexture tex; *)
+    (*
+      Nova.prepareScene w h;
+    Nova.bindTexture tex; *)
 
     (* Nova.withFramebuffer fb (fun () ->
-     *     Cairo.Surface.flush i;
-     *     Cairo.Surface.finish i;
      *   ); *)
+
+    Cairo.Surface.flush i;
+    (* Cairo.Surface.finish i; *)
+    
 
     (* Nova.drawModel gID; *)
     
     System.endFrame s;
   done;
 
+  Cairo.PNG.write i "novapilot_cli.png";
   System.shutdown s
     
