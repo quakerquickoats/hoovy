71a86dbc45f8949e06d80bd287e2d11be1af47e1
Lyndon Tremblay <quakerquickoats@gmail.com>
2019-05-24 21:22:29 -0400

    fub

--- nova/pilot/pilot_cli.ml
+++ nova/pilot/pilot_cli.ml
@@ -3,12 +3,9 @@
   (c) 2019 Lyndon Tremblay
  *)
 
-let () = print_endline "duffhfhfhfhfhfhhfhfhfhfhf"
-
 module System = Nova.System.Make(Nova.System_raylib)
 
 let () =
-  print_endline "duifaiudhfasdf";
   System.run "Novapilot" (module Pilot : Nova.Engine.Game)
 
            
